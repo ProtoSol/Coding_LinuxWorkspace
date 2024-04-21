9970ea01d7ec9826ef6d0113608e49613d71a43f
Pranshu Saini <ps1442003@outlook.com>
2024-04-21 23:36:23 +0530

    Recursion Practice

---
+++ Recursion/PatternRecursive.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class PatternRecursive {
+    public static void main(String[] args) {
+        printLine(5);
+    }
+    private static void printLine(int n){
+        if(n == 0){
+            return;
+        }
+        printLine(n-1);
+        printStar(n);
+        System.out.println();
+    }
+    private static void printStar(int n){
+        if (n == 0) {
+            return;
+        }
+        System.out.print("*");
+        printStar(n-1);
+    }
+}
