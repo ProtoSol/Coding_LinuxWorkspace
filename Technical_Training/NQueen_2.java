c680c5c491d53193dfb6dd707bc4f328d7cb7e82
Pranshu Saini <ps1442003@outlook.com>
2024-04-21 13:43:36 +0530

    Backtracking question

---
+++ Technical_Training/NQueen_2.java
@@ -0,0 +1,63 @@
+package Technical_Training;
+
+public class NQueen_2 {
+    public static void main(String[] args) {
+        int n = 4; // You can change this value to test with different board sizes
+        Solution solution = new Solution();
+        int totalSolutions = solution.totalNQueens(n);
+        System.out.println("Total solutions for " + n + " queens: " + totalSolutions);
+    }
+}
+
+class Solution {
+    boolean[][] board;
+
+    public int totalNQueens(int n) {
+        board = new boolean[n][n]; // Fill with false by default
+        return helper(0, n);
+    }
+
+    private boolean willIPlaceQueen(int row, int col) {
+        // Above Row case
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // Left upper Diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j]) {
+                return false;
+            }
+        }
+
+        // Right Upper Diagonal
+        for (int i = row, j = col; i >= 0 && j < board[0].length; i--, j++) {
+            if (board[i][j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int helper(int row, int n) {
+        // Base Case
+        if (row == n) {
+            return 1;
+        }
+
+        int count = 0;
+        for (int col = 0; col < n; col++) {
+            if (willIPlaceQueen(row, col)) {
+                // place the Queen
+                board[row][col] = true;
+                count += helper(row + 1, n);
+                // Backtrack (Stack Fall)
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+}
