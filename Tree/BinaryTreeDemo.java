f91f932d83dfa23141b09576785e757f8648c385
Pranshu Saini <ps1442003@outlook.com>
2024-07-09 17:28:25 +0530

    Learning About DFT Traversal

--- Tree/BinaryTreeDemo.java
+++ Tree/BinaryTreeDemo.java
@@ -15,16 +15,21 @@ class BinaryTreeOperations{
     Scanner scanner = new Scanner(System.in);
     BinaryTreeNode<Integer> add(){
         System.out.println("Enter the node data in a tree for "+nodeName);
+        // Get the Data
         int data = scanner.nextInt();
+        // Base Case
         if(data == -1){
             return null;
         }
         BinaryTreeNode<Integer> newNode = new BinaryTreeNode<>(data);
+        // Get the Left
         nodeName = "left";
         newNode.left = add();
+        // Get the Right
         nodeName = "right";
         newNode.right = add();
         nodeName = "parent";
+        // Go Back
         return newNode;
 
     }
@@ -53,12 +58,57 @@ class BinaryTreeOperations{
         print(root.left);
         print(root.right);
     }
+
+    // There are three type of Depth First Search Traversal
+        // preOrder (PLR)
+        // inOrder (LPR)
+        // PostOrder (LRP)
+
+    void preOrderPrint(BinaryTreeNode<Integer> parent){
+        // Base Case
+        if(parent == null){
+            return;
+        }
+        // Print Parent
+        System.out.println(parent.data + " ");
+        // Move to left
+        preOrderPrint(parent.left);
+        // Move to Right
+        preOrderPrint(parent.right);
+    }
+
+    void inOrderPrint(BinaryTreeNode<Integer> parent){
+        // Base Case
+        if(parent == null){
+            return;
+        }
+        // Move to left
+        preOrderPrint(parent.left);
+        // Print Parent
+        System.out.println(parent.data + " ");
+        // Move to Right
+        preOrderPrint(parent.right);
+    }
+
+    void postOrderPrint(BinaryTreeNode<Integer> parent){
+        // Base Case
+        if(parent == null){
+            return;
+        }
+        // Move to Left
+        preOrderPrint(parent.left);
+        // Move to Right
+        preOrderPrint(parent.right);
+        // Print Parent
+        System.out.println(parent.data + " ");
+    }
 }
 public class BinaryTreeDemo {
     public static void main(String[] args) {
         BinaryTreeOperations opr = new BinaryTreeOperations();
         BinaryTreeNode<Integer> root = opr.add();
         opr.print(root);
-
+        System.out.println();
+        opr.preOrderPrint(root);
     }
 }
