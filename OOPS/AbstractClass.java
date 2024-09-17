37fa597560abb47e269e42c54e1d8fd48d405d8c
Pranshu Saini <ps1442003@outlook.com>
2024-09-17 13:06:14 +0530

    Testing

--- OOPS/AbstractClass.java
+++ OOPS/AbstractClass.java
@@ -44,5 +44,6 @@ public class AbstractClass {
     public static void main(String[] args) {
         // FullTimeEmployee ram = new FullTimeEmployee();
         // PartTimeEmployee shyam = new PartTimeEmployee();
+        System.out.println("Testing");
     }
 }
